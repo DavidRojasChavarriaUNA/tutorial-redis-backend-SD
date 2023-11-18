6c7a7d0d0b74eaaf864cd7fd784bfe76e649e5e0
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-11-18 03:55:01 -0600

    backend
    backend

--- netlify/functions/bookDelete.js
+++ netlify/functions/bookDelete.js
@@ -1,14 +1,18 @@
 "use strict"
-//const clientPromise = require('./mongoDB');
+const redis = require('./redisDB');
 const headers = require('./headersCORS');
 exports.handler = async (event, context) => {
   if (event.httpMethod == "OPTIONS") {
     return { statusCode: 200, headers, body: "OK" };
   }
   try {
-    /*const client = await clientPromise;
-    const id = parseInt(event.path.split("/").reverse()[0]);
-    await client.db("bookstore").collection("books").deleteOne({id:id});*/
+    const id = event.path.split("/").reverse()[0];
+
+    redis.on("connect", function() {
+      console.log("You are now connected");
+    });
+    await redis.del('book_'+id);
+
     return { statusCode: 200, headers, body: 'OK'};
   } catch (error) {
     console.log(error);

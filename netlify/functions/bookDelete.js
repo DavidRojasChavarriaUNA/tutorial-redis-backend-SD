90b8d0a8266af638a86fa350554223c1de733cfd
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-11-18 02:43:30 -0600

    quitar cosas de mongo
    quitar cosas de mongo

--- netlify/functions/bookDelete.js
+++ netlify/functions/bookDelete.js
@@ -1,14 +1,14 @@
 "use strict"
-const clientPromise = require('./mongoDB');
+//const clientPromise = require('./mongoDB');
 const headers = require('./headersCORS');
 exports.handler = async (event, context) => {
   if (event.httpMethod == "OPTIONS") {
     return { statusCode: 200, headers, body: "OK" };
   }
   try {
-    const client = await clientPromise;
+    /*const client = await clientPromise;
     const id = parseInt(event.path.split("/").reverse()[0]);
-    await client.db("bookstore").collection("books").deleteOne({_id:id});
+    await client.db("bookstore").collection("books").deleteOne({_id:id});*/
     return { statusCode: 200, headers, body: 'OK'};
   } catch (error) {
     console.log(error);
